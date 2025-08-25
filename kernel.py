460e157a16acaad871aecbccd29d72d69cdbf37b
Zawadi <g.zawadi.w@gmail.com>
2025-08-25 14:38:44 +0300

    theorem

--- kernel.py
+++ kernel.py
@@ -1,4 +1,5 @@
-""" kernel.py """
+"""kernel.py"""
+
 from genham import hammersley
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,48 +8,58 @@ from matplotlib import cm
 from numpy.linalg import norm
 
 import numpy as np
-def peaks(x,y):
-    z =  (3*(1-x)**2 * np.exp(-(x**2) - (y+1)**2) 
-          - 10*(x/5 - x**3 - y**5) * np.exp(-x**2 - y**2)
-          - 1/3 * np.exp(-(x+1)**2 - y**2)) 
-    return(z)
- 
-n = 20
-x = -3 + 6*hammersley([2,3],n)
-z = peaks(x[:,0],x[:,1])
-xx, yy = np.mgrid[-3:3:150j,-3:3:150j]
-zz = peaks(xx,yy)
-plt.contour(xx,yy,zz,levels=50)
 
-fig=plt.figure()
-ax = fig.add_subplot(111,projection='3d')
-ax.plot_surface(xx,yy,zz,rstride=1,cstride=1,color='c',alpha=0.3,linewidth=0)
-ax.scatter(x[:,0],x[:,1],z,color='k',s=20)
+
+def peaks(x, y):
+    z = (
+        3 * (1 - x) ** 2 * np.exp(-(x**2) - (y + 1) ** 2)
+        - 10 * (x / 5 - x**3 - y**5) * np.exp(-(x**2) - y**2)
+        - 1 / 3 * np.exp(-((x + 1) ** 2) - y**2)
+    )
+    return z
+
+
+n = 20
+x = -3 + 6 * hammersley([2, 3], n)
+z = peaks(x[:, 0], x[:, 1])
+xx, yy = np.mgrid[-3:3:150j, -3:3:150j]
+zz = peaks(xx, yy)
+plt.contour(xx, yy, zz, levels=50)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.plot_surface(xx, yy, zz, rstride=1, cstride=1, color="c", alpha=0.3, linewidth=0)
+ax.scatter(x[:, 0], x[:, 1], z, color="k", s=20)
 plt.show()
 
+
 def kernel_train(k, n, x, y):
-    K = np.zeros((n,n))
+    K = np.zeros((n, n))
     for i in range(n):
         for j in range(n):
-            K[i,j] = k(x[i,:],x[j])
-    alpha = np.linalg.solve(K@K.T, K@y)
+            K[i, j] = k(x[i, :], x[j])
+    alpha = np.linalg.solve(K @ K.T, K @ y)
     return K, alpha
 
-sig2 = 0.3 # kernel parameter
-def k(x,u):
-   return(np.exp(-0.5*norm(x- u)**2/sig2))
+
+sig2 = 0.3  # kernel parameter
+
+
+def k(x, u):
+    return np.exp(-0.5 * norm(x - u) ** 2 / sig2)
+
+
 K, alpha = kernel_train(k, n, x, z)
 
 
-
-N, = xx.flatten().shape
-Kx = np.zeros((n,N))
+(N,) = xx.flatten().shape
+Kx = np.zeros((n, N))
 for i in range(n):
     for j in range(N):
-        Kx[i,j] = k(x[i,:],np.array([xx.flatten()[j],yy.flatten()[j]]))
-    
+        Kx[i, j] = k(x[i, :], np.array([xx.flatten()[j], yy.flatten()[j]]))
+
 
 g = Kx.T @ alpha
 dim = np.sqrt(N).astype(int)
-yhat = g.reshape((dim,dim))
-plt.contour(xx,yy,yhat,levels=50)
+yhat = g.reshape((dim, dim))
+plt.contour(xx, yy, yhat, levels=50)
